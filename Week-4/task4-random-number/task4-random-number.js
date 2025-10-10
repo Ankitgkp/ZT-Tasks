c449888991a60ed381bf9054f76dd2df1c38d3e5
Personal <personal@Ankits-MacBook-Pro-5.local>
2025-10-10 21:48:37 +0530

    bug

--- Week-4/task4-random-number/task4-random-number.js
+++ Week-4/task4-random-number/task4-random-number.js
@@ -1,7 +1,7 @@
 let guess = prompt("Guess a number (1-10):")
-
+let secret = 4
 if (Number(guess) === secret) {
   alert("Correct!")
 } else {
-  alert("Wrong! The number was" + secret)
+  alert("Wrong! The number was: " + secret)
 }
