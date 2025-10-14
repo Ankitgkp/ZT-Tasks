28bed03680420d9e105dbfe99ff55d9d71d83ae6
Personal <personal@Ankits-MacBook-Pro-5.local>
2025-10-14 14:03:05 +0530

    json

---
+++ Week-6/dummy-json.js
@@ -0,0 +1,9 @@
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+fetch(url)
+  .then(response=> response.json())
+  .then(data=>{
+    console.log(data);
+  })
+  .catch(error=> {
+    console.error('Error')
+  })
